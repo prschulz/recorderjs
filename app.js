24d23d454e34a74b6c548c4515a15f6d0b7121c0
prschulz <prschulz88@gmail.com>
2014-12-08 15:38:52 -0800

    setting up node project

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.render('home');
+});
+
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
+
